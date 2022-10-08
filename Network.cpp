a177e3f4e6b5f73e6b3c0cfbe0f96364f009aa5d
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-10-08 16:50:01 -0400

    Create Network.cpp

---
+++ Network.cpp
@@ -0,0 +1,47 @@
+#include "Network.hpp"
+/*
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Account.hpp"
+//#include "Post.hpp"
+*/
+
+
+
+
+/** default constructor**/
+template<class T>
+Network<T>::Network(): item_count_(0) //Posts.clear()
+{
+}  // end default constructor
+
+
+
+
+/**
+ @return item_count_ : the current size of the network (number of items in the array)
+ **/
+template<class T>
+int Network<T>::getSizeOfNetwork() const
+{
+	return item_count_;
+}  // end getSizeOfNetwork
+
+
+
+
+
+
+/**
+ @return bool -  true if the bag is empty, false otherwise
+ **/
+template<class T>
+bool Network<T>::isEmpty() const
+{
+	return item_count_ == 0;
+}  // end isEmpty
+
+
+
